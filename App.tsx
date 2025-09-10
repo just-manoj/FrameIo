a470d4c4c7b73900a87b19a0e6cf3c5b3f2b18cf
just-manoj <infinity.bot.vision@gmail.com>
2025-09-10 21:40:50 +0530

    REFACTOR: remove unused import

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 import AppNavigation from './src/navigation/AppNavigation';
